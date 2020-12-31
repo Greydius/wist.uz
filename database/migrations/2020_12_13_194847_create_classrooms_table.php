c80cc0eb29dd25094eaa334d4c9fb0e51e570db5
Greydius <mr.greydius@gmail.com>
2020-12-31 08:22:32 +0500

    classrooms migration

---
+++ database/migrations/2020_12_13_194847_create_classrooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger("year")->unsigned();
+            $table->tinyInteger("grade")->unsigned();
+            $table->char("symbol");
+            $table->tinyInteger("limit")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classrooms');
+    }
+}
